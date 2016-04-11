98940fa8245edfc80c7989be3cfdc8c60ea81ae3
Bhaskar Goyal <bhaskargoyal24@gmail.com>
2016-04-11 11:58:07 +0530

    Made basic form html

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	
+	
+?>	
+<!DOCtype html>
+<html>
+<head>
+	<title>Blogging System | Home Page</title>
+	<link rel = "stylesheet" href="css/style.css"/ type="text/css">
+</head>
+<body>
+	<form method="post" action="index.php">
+		<br><br><br><center>
+		Username:
+		<input type= "text" name="username"/>
+		<br><br>
+		Password:
+		<input type="password" name="password"/>
+		<br><br>
+		<input type="submit" name="submit" value="Sign In"></center>
+	</form>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
