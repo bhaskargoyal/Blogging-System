ff0541a53727bafa09016b432793e8883bc97600
Arjun Sharma <arjun.sssp@gmail.com>
2016-04-16 17:24:32 +0530

    Added files via upload

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 	require_once('./connect.php');
 	if(isset($_POST['submit'])){
 		// check for username and password
-		$username = strtolower($_POST['username']);
+		$username = $_POST['username'];
 		$password = $_POST['password'];
 		// check from db
 		$query = 'SELECT username, password FROM login WHERE username LIKE \''.$username.'\' AND password LIKE \''.$password.'\';';
 		$result = mysqli_query($con, $query);
 		if(mysqli_num_rows($result) == 1){
 			$row = mysqli_fetch_assoc($result);
-			if($username == strtolower($row['username']) && $password == $row['password']){
+			if($username == $row['username'] && $password == $row['password']){
 				mysqli_close();
 				$_SESSION['wrong'] = "";
 				$_SESSION['login'] = 1;
@@ -37,19 +37,36 @@
 <head>
 	<title>Blogging System | Index Page</title>
 	<link rel = "stylesheet" href="css/style.css"/ type="text/css">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<form method="post" action="index.php">
-		<br><br><br><center>
-		Username:
-		<input type= "text" name="username" required />
-		<br><br>
-		Password:
-		<input type="password" name="password" required/>
-		<br><br>
-		<input type="submit" name="submit" value="Sign In">
-	</form>
-	<br>
+    <div id="area1">
+            <div id="signup"><button type="button" class="btn btn-default btn-block"><a href="newuser.php">New User</a></button></div>
+        </div>
+    <div id="area2">
+        <img src="images/message1.png" id="messageleft">
+        <img src="images/message1.png" id="messageright">
+    <div id="loginarea">
+        <div class="container">
+        <h2 id="heading">Registered user? Login!</h2>
+    <form role="form" id="actualarea" method="post" action="index.php">
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" name="username" class="form-control" id="username" placeholder="Enter email"required />
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" name="password" class="form-control" id="pwd" placeholder="Enter password" required/>
+        </div>
+        <div class="checkbox">
+            <label><input type="checkbox"> Remember me</label>
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary" id="submitbutton" value="Sign In">Login</button>
+  </form>
 	<h2><?php 
 		if(isset($_SESSION['wrong'])){
 			echo '<br>'.'<h4>'.$_SESSION['wrong'].'</h4>';
@@ -57,33 +74,8 @@
 		}
 	?></h2>
 	<br>
-	<h4><a href="newuser.php">New User</a></h4>
+	
 	</center>
-	<!-- all blogs on db  -->
-	<?php
-		$query = "SELECT * FROM blogs;";
-		$result = mysqli_query($con, $query);
-		if(mysqli_num_rows($result)  == 0){
-			echo "<br>"."<h3>No Blogs Found</h3>";
-		} else {
-			while($row = mysqli_fetch_assoc($result)){
-				echo "<h2><a href=\"blog.php?blog=".$row['id']."\" target=\"_blank\">".$row['heading']."</a></h2>";
-				echo "<h3>".$row['subheading']."</h3>";
-				echo "<p class=\"truncate\">".$row['text']."</p>";
-				$query = 'SELECT firstname, lastname, age, username FROM users WHERE id = '.$row['user_id'].';';
-				$rre = mysqli_query($con, $query);
-				if(mysqli_num_rows($rre) == 1){
-					$rr = mysqli_fetch_assoc($rre);
-					echo "<p>Written by <b>".$rr['firstname']." ".$rr['lastname']."</b>, Age ".$rr['age'].".</p>";
-					echo "<p>".$row['time']."</p>";
-					echo "<br>";
-				} 
-			}
-		}
-		
-
-
-	?>
 	<script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
